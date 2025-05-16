cc2a6415eb94f01bb90b9e8eecbe125c05cfc019
Iwazaki Shota <csso24001@g.nihon-u.ac.jp>
2025-05-16 20:37:11 +0900

    pagesの追加とマルチページ作成

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import home
-import experiment
-import presentation_today
-import presentation_history
-import dataset
-import program
-import data_analysis
-import save_experiment
-import questionnaire
 
 with st.sidebar:
     selected = option_menu(
@@ -23,29 +14,29 @@ with st.sidebar:
     )
 
 # 選択されたページに応じてコンテンツを表示
-if selected == 'ホーム':
-    home.show()
+# if selected == 'ホーム':
+#     home.show()
 
-elif selected == '実験':
-    experiment.show()
+# elif selected == '実験':
+#     experiment.show()
 
-elif selected == '本日の発表':
-    presentation_today.show()
+# elif selected == '本日の発表':
+#     presentation_today.show()
 
-elif selected == '発表履歴':
-    presentation_history.show()
+# elif selected == '発表履歴':
+#     presentation_history.show()
 
-elif selected == 'データセット':
-    dataset.show()
+# elif selected == 'データセット':
+#     dataset.show()
 
-elif selected == 'プログラム':
-    program.show()
+# elif selected == 'プログラム':
+#     program.show()
 
-elif selected == 'データ分析':
-    data_analysis.show()
+# elif selected == 'データ分析':
+#     data_analysis.show()
 
-elif selected == '実験結果の保存':
-    save_experiment.show()
+# elif selected == '実験結果の保存':
+#     save_experiment.show()
 
-else:
-    questionnaire.show()
+# else:
+#     questionnaire.show()
