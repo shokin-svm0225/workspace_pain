8fd28fb6a16f04a7f6a1cd8e56a6f4c03faa5552
Iwazaki Shota <csso24001@g.nihon-u.ac.jp>
2025-05-18 05:09:24 +0900

    streamlitの説明追加

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-st.title('データ加工・機械学習のプログラムを表示')
+st.title('streamlit')
+
+st.header('streamlitのインストール', divider='rainbow')
+# コード表示
+code = '''pip install streamlit'''
+st.code(code, language='python')
+
+st.header('プログラムの簡単な例', divider='rainbow')
+# コード表示
+code = '''import streamlit as st
+        st.title('こんにちは、Streamlit！')
+        st.write('これが私の最初のStreamlitアプリです。')'''
+st.code(code, language='python')
+
+st.header('実行コード', divider='rainbow')
+# コード表示
+code = '''streamlit run app.py'''
+st.code(code, language='python')
