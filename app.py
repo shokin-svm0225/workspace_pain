240e94ed00deb654fdfb0836772db8b00585bc65
Iwazaki Shota <csso24001@g.nihon-u.ac.jp>
2025-05-18 05:12:26 +0900

    subheaderに変更

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from streamlit_option_menu import option_menu
 
 st.title('streamlit')
 
-st.header('streamlitのインストール', divider='rainbow')
+st.subheader('streamlitのインストール', divider='rainbow')
 # コード表示
 code = '''pip install streamlit'''
 st.code(code, language='python')
 
-st.header('プログラムの簡単な例', divider='rainbow')
+st.subheader('プログラムの簡単な例', divider='rainbow')
 # コード表示
 code = '''import streamlit as st
         st.title('こんにちは、Streamlit！')
         st.write('これが私の最初のStreamlitアプリです。')'''
 st.code(code, language='python')
 
-st.header('実行コード', divider='rainbow')
+st.subheader('実行コード', divider='rainbow')
 # コード表示
 code = '''streamlit run app.py'''
 st.code(code, language='python')
